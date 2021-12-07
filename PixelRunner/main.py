39ff661ff7ac74ca44da748f0f8706748fcde1fb
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-06 17:45:57 -0800

    Displaying images (with surfaces)

--- PixelRunner/main.py
+++ PixelRunner/main.py
@@ -5,13 +5,21 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('PixelRunner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('PixelRunner\\font\Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('PixelRunner\graphics\Sky.png')
+ground_surface = pygame.image.load('PixelRunner\graphics\ground.png')
+text_surface = test_font.render('My game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update elements
+
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
     pygame.display.update()
     clock.tick(60)
