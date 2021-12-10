11a87c9e1ce46129aa61ad751c40726de1f05df6
ajfm88 <ajfoucaultmo@mail.usf.edu>
2021-12-10 00:54:45 -0800

    Drawing with rectangles (+colors)

--- PixelRunner/main.py
+++ PixelRunner/main.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font('PixelRunner\\font\Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('PixelRunner\graphics\Sky.png').convert()
 ground_surface = pygame.image.load('PixelRunner\graphics\ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surf = test_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('PixelRunner\graphics\snail\snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600, 300)) # Bottom of the snail has to be in the same position as ground_surface
@@ -27,7 +29,9 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
+    pygame.draw.rect(screen,'#c0e8ec',score_rect)
+    pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+    screen.blit(score_surf,score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800     #In pygame you don't move the surface, you move the rectangle that contains the surface
