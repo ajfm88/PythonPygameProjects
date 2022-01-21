1f82de432c81bbac1f08baabaea2f09843fd3369
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-20 16:59:36 -0800

    Setting up the level

--- TreasureHunters/main.py
+++ TreasureHunters/main.py
@@ -1,12 +1,13 @@
+from cgi import test
 import pygame, sys
 from settings import *
+from level import Level
 
 # Pygame setup
 pygame.init()
-screen_width = 1200
-screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map,screen)
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +16,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
