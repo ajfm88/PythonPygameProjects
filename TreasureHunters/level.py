86b6ce531ac5df93b5b7c356cbfb77433b2a2fca
quasar098 <patellmlg@gmail.com>
2022-01-25 19:52:19 -0500

    more pep 8 convention

--- TreasureHunters/level.py
+++ TreasureHunters/level.py
@@ -3,28 +3,35 @@ from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
 
+
 class Level:
-    def __init__(self,level_data,surface):
+    def __init__(self, level_data, surface):
 
         # level setup
         self.display_surface = surface
         self.setup_level(level_data)
         self.world_shift = 0
+        self.tiles = None
+        self.player = None
 
-    def setup_level(self,layout):
+    def get_player(self) -> Player:
+        # noinspection PyTypeChecker
+        return self.player.sprites()[0]
+
+    def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
 
-        for row_index,row in enumerate(layout):
-            for col_index,cell in enumerate(row):
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
                 x = col_index * tile_size
                 y = row_index * tile_size
 
                 if cell == 'X':
-                    tile = Tile((x,y),tile_size)
+                    tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x,y))
+                    player_sprite = Player((x, y))
                     self.player.add(player_sprite)
 
     def scroll_x(self):
