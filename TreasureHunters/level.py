71aaaae0a067a1b08d2548cd6d94935bbcdb3dcc
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-27 22:51:21 -0800

    Dust particle animations - jumping

--- TreasureHunters/level.py
+++ TreasureHunters/level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
+from particles import ParticleEffect
 
 class Level:
     def __init__(self,level_data,surface):
@@ -11,6 +12,17 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        # dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+
+    def create_jump_particles(self,pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10,5)
+        else:
+            pos += pygame.math.Vector2(10,-5)            
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
 
     def setup_level(self,layout):
         self.tiles = pygame.sprite.Group()
@@ -25,7 +37,7 @@ class Level:
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x,y),self.display_surface)
+                    player_sprite = Player((x,y),self.display_surface,self.create_jump_particles)
                     self.player.add(player_sprite)
 
     def scroll_x(self):
@@ -84,6 +96,9 @@ class Level:
             player.on_ceiling = False
 
     def run(self):
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
 
         #level tiles
         self.tiles.update(self.world_shift)
