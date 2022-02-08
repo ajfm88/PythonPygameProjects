cbab318a42ec648ba3d138abfd598afee20741f6
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-02-08 15:22:46 -0800

    Refactor Treasure Hunters Code

--- TreasureHunters/code/main.py
+++ TreasureHunters/code/main.py
@@ -1,21 +1,22 @@
 import pygame, sys
-from settings import * 
+from settings import *
 from level import Level
+from game_data import level_0
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map,screen)
+level = Level(level_0,screen)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-	
-	screen.fill('black')
-	level.run()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	pygame.display.update()
-	clock.tick(60)
+    screen.fill('black')
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
